a85acac824774ef54142971e4c666fdfeabe17d0
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-11-14 13:03:51 -0700

    B_02_building-vue-js-view-pages
    faf

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '@/views/Home.vue'
+import Dashboard from "@/views/Dashboard.vue";
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/Dashboard',
+    name: 'Dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Dashboard
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
 const router = createRouter({
