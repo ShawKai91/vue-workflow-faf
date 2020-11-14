56708867fcdebf4505ee86e1da101d2ce4652792
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-11-14 13:22:55 -0700

    B_03_vue-presentation-components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/Dashboard">Dashboard</router-link>
+    <router-link to="/dashboard">Dashboard</router-link>
   </div>
   <router-view/>
 </template>
